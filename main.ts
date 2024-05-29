974b0ac7b225406de2c2549ed5b53ea13ff4698a
AsadullahNagori <164639593+AsadullahNagori@users.noreply.github.com>
2024-05-29 16:03:49 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,6 @@
+let Guest_List: string[] = ["Asad", "Ahmed", "Aliyan"];
+
+//for (let i = 0; i < Guest_List.length; i++) {
+ // console.log(`Dear ${Guest_List[i]} :\n you are invited to dinner!\n`);
+//}
+console.log(`Total number of people invited: ${Guest_List.length}`);
